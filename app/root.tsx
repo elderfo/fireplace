4337becfa030fe54b7a92d73d88258ce1943e93a
Christopher Getsfred <cgetsfred@gmail.com>
2022-01-29 18:02:29 +0000

    tailwind

--- app/root.tsx
+++ app/root.tsx
@@ -4,12 +4,17 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration
-} from "remix";
-import type { MetaFunction } from "remix";
+  ScrollRestoration,
+} from 'remix';
+import type { MetaFunction } from 'remix';
+import styles from './tailwind.css';
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
 
 export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
+  return { title: 'New Remix App' };
 };
 
 export default function App() {
@@ -21,11 +26,11 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body className="w-screen h-screen bg-gray-100 p-2 overflow-hidden">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
-        {process.env.NODE_ENV === "development" && <LiveReload />}
+        {process.env.NODE_ENV === 'development' && <LiveReload />}
       </body>
     </html>
   );
